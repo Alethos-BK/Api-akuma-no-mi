c5d188d72686d675b591bb3bcf611d6e13b14f9a
Alice Periles <odahcamperiles@gmail.com>
2021-07-12 15:32:41 -0300

    emails

---
+++ src/main/java/com/onepiece/akumanomi/email/MensagemEmail.java
@@ -0,0 +1,43 @@
+package com.onepiece.akumanomi.email;
+
+import java.util.List;
+
+public class MensagemEmail {
+    
+    private String assunto;
+    private String corpo;
+    private List<String> destinatarios;
+    
+    public MensagemEmail(String assunto, String corpo, List<String> destinatarios) {
+        this.assunto = assunto;
+        this.corpo = corpo;
+        this.destinatarios = destinatarios;
+    }
+
+    public String getAssunto() {
+        return assunto;
+    }
+
+    public void setAssunto(String assunto) {
+        this.assunto = assunto;
+    }
+
+    public String getCorpo() {
+        return corpo;
+    }
+
+    public void setCorpo(String corpo) {
+        this.corpo = corpo;
+    }
+
+    public List<String> getDestinatarios() {
+        return destinatarios;
+    }
+
+    public void setDestinatarios(List<String> destinatarios) {
+        this.destinatarios = destinatarios;
+    }
+
+    
+    
+}
